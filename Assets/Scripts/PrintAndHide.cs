c25fe1408f399a05ae1740a0812b8c57e7d597a9
hunter-blaze <captainjackoneil@gmail.com>
2021-08-29 22:58:26 +1000

    100 percent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -8,17 +8,22 @@ public class PrintAndHide : MonoBehaviour
     public Renderer rend;
     
     int i = 3;
+    int randomValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        randomValue = Random.Range(200, 250);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
+        if (obj.tag == "Red" && i == 100)
+            obj.SetActive(false);
+        if (obj.tag == "Blue" && i == randomValue)
+            rend.enabled  = false;
         Debug.Log(obj.name + ":" + i);
     }
 }
